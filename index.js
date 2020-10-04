e03af5a321f03bf087a1842d2ce309f7be505c2a
c:\csanders <71975541+colburncodes@users.noreply.github.com>
2020-10-04 11:04:14 -0700

    init create

---
+++ index.js
@@ -0,0 +1,35 @@
+$(function () {
+  // submit function to handle the event
+  $("#js-shopping-list-form").submit(function (event) {
+    // alert("Handler for .submit() called.");
+    event.preventDefault();
+    const items = $("#shopping-list-entry").val(); // list of shopping items
+
+    $("#shopping-list-entry").val(""); // reads list entry
+    // add item to the list
+    $(".shopping-list").append(
+      `<li>
+          <span class="shopping-item">${items}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`
+    );
+    // targeting the closest list item
+    $(".shopping-list").on("click", ".shopping-item-delete", function (event) {
+      $(this).closest("li").remove();
+    });
+    // targeting the closet list item.
+    $(".shopping-list").on("click", ".shopping-item-toggle", function (event) {
+      $(this)
+        .closest("li")
+        .find(".shopping-item")
+        .toggleClass("shopping-item__checked");
+    });
+  });
+});
